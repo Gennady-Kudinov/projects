ff3d4977cb2af809e5decfd693400952743df002
Gennady Kudinov <gennady71@gmail.com>
2022-07-02 07:39:10 +0300

    команды запроса и передачи тест

--- Servis/test.rb
+++ Servis/test.rb
@@ -2,6 +2,12 @@ require 'SQlite3'
 	
 db = SQLite3::Database.new 'brand.db3'
 
+db.execute "SELECT * from Brandauto" do |name|
+    puts name
+    puts "===="
+end
 #       Выполним обращение к базе данных
-db.execute "INSERT INTO Brandauto (id, name, brand) VALUES (1, kia, KIA)"
+db.execute "INSERT INTO Brandauto (name, brand) VALUES ('great wall', 'GREAT WALL'), ('mitsubishi', 'MITSUBISHI')"
+
+
 db.close
